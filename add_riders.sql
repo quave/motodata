1e113a09aca854aa58f5c83a797c935a238ed168
Quave <vladsynkov@gmail.com>
2016-04-17 22:09:12 +0300

    fixes

--- add_riders.sql
+++ add_riders.sql
@@ -1,5 +1,4 @@
 insert into people (first_name, last_name, country) values ('Tekkyu', 'Kayoh', 'JPN');
 insert into people (first_name, last_name, country) values ('Jason', 'Vincent', 'GBR');
 insert into people (first_name, last_name, country) values ('Chris', 'Peris', 'USA');
-insert into people (first_name, last_name, country) values ('Muhammad', 'Zulfa ', 'MAL');
-
+insert into people (first_name, last_name, country) values ('Muhammad', 'ZULFA ', 'MAL');
